ebf44613f5a2b5accf7d8b4852009bf635d5039d
pedroluccs <130405548+pedroluccs@users.noreply.github.com>
2023-08-10 20:00:01 -0400

    Add files via upload
    duvidas

---
+++ main.js
@@ -0,0 +1,32 @@
+const form =document.getElementById('form-deposito');
+
+function validaNome (nomeCompleto) {
+    const nomeComoArray = nomeCompleto.split(' ');
+    return nomeComoArray.length >= 2;
+}
+
+form.addEventListener('submit', function(e) {
+    let formEValido = false;
+    e.preventDefault();
+
+    const nomeBeneficiario = document.getElementById('nome-beneficiario');
+    const numeroContaBeneficiario = document.getElementById('numero-conta');
+    const valorDeposito = document.getElementById ('valor-deposito');
+    const mensagemSucesso = `Montante de: <b>${valorDeposito.value}</b> depositado para o cliente: <b>${nomeBeneficiario.value}</b> - conta: <b>${numeroContaBeneficiario.value}`;
+    
+    formEValido = validaNome(nomeBeneficiario.value)
+    if (formEValido) {  
+        const containerMensagemSucesso = document.querySelector('.sucess-message');
+        containerMensagemSucesso.innerHTML = mensagemSucesso;
+        containerMensagemSucesso.style.display = 'block';
+
+        nomeBeneficiario.value = '';
+        numeroContaBeneficiario.value = '';
+        valorDeposito.value = '';
+        
+    }  else {
+        nomeBeneficiario.style.border = '1px solid red';
+        document.querySelector('.error-message').style.display = 'block';
+    }
+})
+
