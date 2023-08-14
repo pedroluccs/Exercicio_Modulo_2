9d20ca5151dd6a3f81d71ac5ee4ed3b246d945f1
pedroluccs <130405548+pedroluccs@users.noreply.github.com>
2023-08-14 19:37:27 -0400

    Update main.js
    exercício feito

--- main.js
+++ main.js
@@ -1,32 +1,38 @@
-const form =document.getElementById('form-deposito');
+const form = document.getElementById('form-compra');
+const numeroIdade = document.getElementById('numero-idade');
 
-function validaNome (nomeCompleto) {
-    const nomeComoArray = nomeCompleto.split(' ');
-    return nomeComoArray.length >= 2;
+function verificaIdade(idade) {
+    return idade >= 18;
 }
 
 form.addEventListener('submit', function(e) {
-    let formEValido = false;
     e.preventDefault();
 
-    const nomeBeneficiario = document.getElementById('nome-beneficiario');
-    const numeroContaBeneficiario = document.getElementById('numero-conta');
-    const valorDeposito = document.getElementById ('valor-deposito');
-    const mensagemSucesso = `Montante de: <b>${valorDeposito.value}</b> depositado para o cliente: <b>${nomeBeneficiario.value}</b> - conta: <b>${numeroContaBeneficiario.value}`;
-    
-    formEValido = validaNome(nomeBeneficiario.value)
-    if (formEValido) {  
-        const containerMensagemSucesso = document.querySelector('.sucess-message');
-        containerMensagemSucesso.innerHTML = mensagemSucesso;
+    const idade = parseInt(numeroIdade.value);
+    const containerMensagemSucesso = document.querySelector('.success-message');
+    const containerMensagemErro = document.querySelector('.error-message');
+
+    if (verificaIdade(idade)) {
+        containerMensagemSucesso.textContent = 'Aguarde e será redirecionado!';
         containerMensagemSucesso.style.display = 'block';
-
-        nomeBeneficiario.value = '';
-        numeroContaBeneficiario.value = '';
-        valorDeposito.value = '';
-        
-    }  else {
-        nomeBeneficiario.style.border = '1px solid red';
-        document.querySelector('.error-message').style.display = 'block';
+        numeroIdade.classList.remove('error');
+        containerMensagemErro.style.display = 'none';
+    } else {
+        numeroIdade.classList.add('error');
+        containerMensagemErro.style.display = 'block';
+        containerMensagemSucesso.style.display = 'none';
     }
-})
+});
 
+numeroIdade.addEventListener('input', function() {
+    const idade = parseInt(numeroIdade.value);
+    const containerMensagemErro = document.querySelector('.error-message');
+
+    if (verificaIdade(idade)) {
+        numeroIdade.classList.remove('error');
+        containerMensagemErro.style.display = 'none';
+    } else {
+        numeroIdade.classList.add('error');
+        containerMensagemErro.style.display = 'block';
+    }
+});
