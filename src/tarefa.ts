ccaae303c8b1c7697c3bb91a737d55bdb6ce2d7c
pedroluccs <130405548+pedroluccs@users.noreply.github.com>
2024-09-22 14:56:49 -0400

    tarefa do modulo de ts

---
+++ src/tarefa.ts
@@ -0,0 +1,15 @@
+// Função de multiplicação
+function multiplicar(a: number, b: number): number {
+    return a * b;
+}
+
+// Função de saudação
+function saudar(nome: string): string {
+    return "Olá " + nome;
+}
+
+const resultadoMultiplicacao = multiplicar(5, 3); 
+const saudacao = saudar("Pedro"); 
+
+console.log(resultadoMultiplicacao);  
+console.log(saudacao); 
